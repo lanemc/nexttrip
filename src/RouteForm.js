ac2b577dc1acaa0406d226b0fa858b368f44ce67
unknown <lanemcunningham@gmail.com>
2019-12-07 20:42:03 -0600

    Api route call

--- src/RouteForm.js
+++ src/RouteForm.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const RouteForm = () => (
-    <>
+const parser = require('xml-js');
 
-    </>
-);
+class RouteForm extends React.Component {
+    state = {
+        routes: [
+            {
+                name: '',
+                number: ''
+            }
+        ]
+    }
+
+    componentDidMount() {
+        fetch("https://svc.metrotransit.org/NexTrip/Routes")
+            .then((response) => response.text())
+            .then(response => {
+                const parseResponse = JSON.parse(parser.xml2json(response, {compact: true, spaces: 4}));
+                console.log(parseResponse.ArrayOfNexTripRoute.NexTripRoute);
+                const res = parseResponse.ArrayOfNexTripRoute.NexTripRoute;
+                
+                const mapRoutes = res.map(route => { return {route: route.Route._text, display: route.Description._text  }});
+
+                console.log(mapRoutes);
+                this.setState({ routes: [{routes: '', display: '(Select a route)'}].concat(mapRoutes) });
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <>
+                <select>
+                    {this.state.routes.map((route) => <option key={route.route} value={route.route}>{route.display}</option>)}
+                </select>
+            </>
+        );
+    }
+
+}
 
 export default RouteForm;
