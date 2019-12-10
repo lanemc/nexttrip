67a73ae2c2107b69bb2ed8e64adeabcddab7865d
unknown <lanemcunningham@gmail.com>
2019-12-09 18:04:36 -0600

    Rendering directions

--- src/RouteSelect.js
+++ src/RouteSelect.js
@@ -1,18 +1,29 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
+import UseForceUpdate from './ForceUpdate';
 
 const parser = require('xml-js');
 
 const RouteSelect = props => {
     const { routes } = props;
     
+    UseForceUpdate();
+
     const [selectedRouteName, setRouteName] = useState('');
     const [selectedRouteDirection, setRouteDirection] = useState('');
 
     //console.log(routes);
 
     const onChangeHandler = event => {
+
         setRouteName({ name: event.target.value });
+
     }
+
+    useEffect(() => {
+        if (selectedRouteName) {
+            getRouteDirection();
+        }
+      }, [selectedRouteName.name]);
 
     const getRouteDirection = () => {
 
@@ -25,7 +36,7 @@ const RouteSelect = props => {
                 return response.text();
             }).then(response => {
                 return JSON.parse(parser.xml2json(response, {compact: true, spaces: 4}));
-            }).then(response => { // offending block
+            }).then(response => {
                 directions = response.ArrayOfTextValuePair.TextValuePair;
                 // console.log(directions);
                 setRouteDirection(directions);    
@@ -33,16 +44,30 @@ const RouteSelect = props => {
             .catch(error => {
                 console.log(error);
             });
+
+        //console.log(selectedRouteDirection[0].text); // This logged state is one value behind the selected option
     }
 
     const routeOptions = routes.map(route => <option key={route.Route._text}>{route.Description._text}</option>);
 
+    let dirs = null;
+    
+    if(selectedRouteDirection) {
+        console.log(selectedRouteDirection);
+        dirs = (
+            <div>
+                {selectedRouteDirection.map((dir, index) => {
+                    return <div key={index}>{dir.Text._text}</div>
+                })}
+            </div>
+        );
+    }
     return (
         <Fragment>
             <select onChange={onChangeHandler}>
                 {routeOptions}
             </select>
-        {selectedRouteName ? getRouteDirection() : null} 
+            {dirs}
         </Fragment>
     );
 };
